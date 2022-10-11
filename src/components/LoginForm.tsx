de79205608f2bb9e8b18f57c53cb98eeac3511c8
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-10-11 19:24:05 +0100

    implented styling for LoginForm Component, added to variable file & adjusted LoginForm structure

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 import '../styles/LoginForm.scss'
 import logo from '../assests/Union.png'
 import signInPic from '../assests/pablo-sign-in 1.png'
+import lendsqr from '../assests/lendsqr.png'
 
 interface LoginProps {
     username: string
@@ -15,31 +16,34 @@ const LoginForm = () => {
                 <div className="login-left">
                     <div className="left-logo">
                         <img src={logo} alt='lendsqr logo' className="logo" />
+                        <img src={lendsqr} alt="lendsqr" className="lendsqr"/>
                     </div>
-                    <div className="signup-image">
+                    <div className="signup-image-container">
                         <img src={signInPic} alt="inner form picture" />
                     </div>
                 </div>
+            </div>
 
-                <div className="login-right">
+            <div className="split right">
+                <div className="login-right centered">
+                    <div className="form-group">
                     <div className="right-heading">
                         <h1>Welcome!</h1>
                         <p>Enter details to login</p>
                     </div>
-                    <div className="form-group">
                         <div className="form-control">
                             <label htmlFor="email" />
                             <input type="email" placeholder="Email" className="form-input" />
                         </div>
                         <div className="form-control">
                             <label htmlFor="password" />
-                            <input type="email" placeholder="Password" className="form-input" />
+                            <input type="password" placeholder="Password" className="form-input" />
                         </div>
-                        <div className="form-control">
-                            <Link to="/register" className="link">Forgot Password?</Link>
+                        <div className="form-control link-container">
+                            <Link to="/forget-password" className="link">Forgot Password?</Link>
                         </div>
                         <div className="button-container">
-                            <button>LOG IN</button>
+                            <button className="submit-btn">LOG IN</button>
                         </div>
                     </div>
                 </div>
