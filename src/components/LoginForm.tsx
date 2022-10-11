a0bb652bdacd110396f07fc2536f0fbacfd24def
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-10-11 08:17:38 +0100

    prep LoginForm logo section and declared some global variable in App.scss file for styling

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,4 +1,4 @@
-
+import { Link } from 'react-router-dom'
 
 interface LoginProps {
     username: string
@@ -6,39 +6,43 @@ interface LoginProps {
 }
 
 const LoginForm = () => {
-  return (
-    <div>
-        <div className="split">
-            <div className="login-left">
-                <div className="left-heading">
-                <h1>Welcome!</h1>
-                <p>Enter details to login</p>
-                </div>
-                <div className="form-group">
-                    <div className="form-control">
-                    <label htmlFor="email"/>
-                    <input type="email" placeholder="Email" className="form-input"/>
+    return (
+        <div>
+            <div className="split">
+                <div className="login-left">
+                    <div className="left-logo">
+                        <img alt='lendsqr logo' />
                     </div>
-                    <div className="form-control">
-                    <label htmlFor="password"/>
-                    <input type="email" placeholder="Password" className="form-input"/>
-                    </div>
-                    <div className="form-control">
-                        
-                    </div>
-                    <div className="button-container">
-                        <button>LOG IN</button>
+                    <div>
+                        <img alt="inner form picture" />
                     </div>
                 </div>
-            </div>
 
-
-            <div className="login-right">
-
+                <div className="login-right">
+                    <div className="right-heading">
+                        <h1>Welcome!</h1>
+                        <p>Enter details to login</p>
+                    </div>
+                    <div className="form-group">
+                        <div className="form-control">
+                            <label htmlFor="email" />
+                            <input type="email" placeholder="Email" className="form-input" />
+                        </div>
+                        <div className="form-control">
+                            <label htmlFor="password" />
+                            <input type="email" placeholder="Password" className="form-input" />
+                        </div>
+                        <div className="form-control">
+                            <Link to="/register" className="link">Forgot Password?</Link>
+                        </div>
+                        <div className="button-container">
+                            <button>LOG IN</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default LoginForm
