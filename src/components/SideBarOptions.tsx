18a06fed28333a4da0fc498d99bb2f0182cd36f3
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-10-14 09:59:22 +0100

    add to global vaiable and setup structure for sidebarOptions

--- src/components/SideBarOptions.tsx
+++ src/components/SideBarOptions.tsx
@@ -3,7 +3,7 @@ import '../styles/SideBars.scss'
 
 
 type IconProps = {
-  text: string,
+  text?: string,
   icon?: JSX.Element,
   active?: string
 }
@@ -17,15 +17,52 @@ const SideBarOptions: React.FC<IconProps> = ({ icon, text, active }) => {
   )
 }
 
-export const SideBar: React.FC<any> = ({}) => {
+export const SideBar: React.FC<any> = () => {
   return (
     <div className="sidebar">
       <select>
-        <option>Switch Organization</option>
+        <option><SideBarOptions text='Switch Organization' icon={<FaHome />} /></option>
       </select>
-     <SideBarOptions text='Dashboard' icon={<FaHome />} />
-     <SideBarOptions text='reel' icon={<FaHome />} />
-     <SideBarOptions text='native' icon={<FaHome />} />
+     <div className="side-section"><SideBarOptions text='Dashboard' icon={<FaHome />} /></div>
+     
+     <div className='wrap customers'>
+     <p className='side-heading'>customers</p>
+     <SideBarOptions text='Users' icon={<FaHome />} />
+     <SideBarOptions text='Guarantors' icon={<FaHome />} />
+     <SideBarOptions text='Loans' icon={<FaHome />} />
+     <SideBarOptions text='Decision Models' icon={<FaHome />} />
+     <SideBarOptions text='Savings' icon={<FaHome />} />
+     <SideBarOptions text='Loan Requests' icon={<FaHome />} />
+     <SideBarOptions text='Whitelist' icon={<FaHome />} />
+     <SideBarOptions text='Karma' icon={<FaHome />} />
+     </div>
+
+     <div className='wrap businesses'>
+     <p className='side-heading'>businesses</p>
+     <SideBarOptions text='Organization' icon={<FaHome />} />
+     <SideBarOptions text='Loan Products' icon={<FaHome />} />
+     <SideBarOptions text='Savings Products' icon={<FaHome />} />
+     <SideBarOptions text='Fees and Charges' icon={<FaHome />} />
+     <SideBarOptions text='Transactions' icon={<FaHome />} />
+     <SideBarOptions text='Services' icon={<FaHome />} />
+     <SideBarOptions text='Services Account' icon={<FaHome />} />
+     <SideBarOptions text='Settlements' icon={<FaHome />} />
+     <SideBarOptions text='Reports' icon={<FaHome />} />
+     </div>
+
+     <div className='wrap settings'>
+     <p className='side-heading'>settings</p>
+     <SideBarOptions text='Preferences' icon={<FaHome />} />
+     <SideBarOptions text='Fees and Pricing' icon={<FaHome />} />
+     <SideBarOptions text='Audit Logs' icon={<FaHome />} />
+     <SideBarOptions text='Systems Messages' icon={<FaHome />} />
+     
+     </div>
+
+    <SideBarOptions icon={<FaHome />} />
+    <div className='logout'>
+      <button>Logout</button>
+    </div>
     </div>
   )
 }
