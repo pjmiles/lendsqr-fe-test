4ab90d699dcbd3c71b7cc8d901db69f5b7d5faec
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-10-15 19:16:01 +0100

    Navbar component setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@ import { Routes, Route } from 'react-router-dom'
 import './App.scss'
 import LoginForm from "./components/LoginForm";
 import { SideBar } from './components/SideBarOptions';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<LoginForm />} />
-        <Route path='/users' element={<SideBar />} />
+        <Route path='/users' 
+        element={
+        <>
+        <NavBar />
+        <SideBar />
+        </>
+        } 
+        />
       </Routes> 
     </div>
   );
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,36 @@
+import logo from '../assests/Union.png'
+import lendsqr from '../assests/lendsqr.png'
+import { NavLink } from 'react-router-dom'
+import { FaHome } from 'react-icons/fa'
+
+
+const NavBar = () => {
+  return (
+    <nav>
+        <div className="logo">
+          <img alt='logo' src={logo} />
+          <img alt='lendsqr' src={lendsqr} />
+        </div>
+        <form className='search'>
+          <input type="search" placeholder='Search for anything' />
+          <button>
+            <i className='search-icon'>
+              {<FaHome />}
+            </i>
+          </button>
+        </form>
+        <div className='group'>
+          <NavLink to="/docs">Docs</NavLink>
+          <div>notification</div>
+          <div className='avatar'>
+            <img alt='avatar' src='' />
+            <select placeholder='Adedeji'>
+              <option>Username</option>
+            </select>
+          </div>
+        </div>
+    </nav>
+  )
+}
+
+export default NavBar
